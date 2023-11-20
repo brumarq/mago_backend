58c194ed7b90f92c032f08261cd9df933200116a
paxer2k <alex.arkhipov.7590@gmail.com>
2023-11-20 19:27:03 +0100

    added endpoints for getting aggregated data and device metrics (moqs)

---
+++ MetricsMS/WebApp/Controllers/MetricsController.cs
@@ -0,0 +1,49 @@
+using Application.ApplicationServices.Interfaces;
+using Domain.Entities;
+using Domain.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("metrics")]
+    [ApiController]
+    public class MetricsController : ControllerBase
+    {
+        private readonly IMetricsService _metricsService;
+
+        public MetricsController(IMetricsService metricsService)
+        {
+            _metricsService = metricsService;
+        }
+
+        [HttpGet("device/{deviceId}")]
+        public async Task<ActionResult<LogCollection>> GetDeviceMetrics(int deviceId)
+        {
+            try
+            {
+                var deviceMetrics = await _metricsService.GetDeviceMetrics(deviceId);
+
+                return Ok(deviceMetrics);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpGet("aggregated-logs/{aggregatedLogDateType}")]
+        public async Task<ActionResult<AggregatedLog>> GetAggregatedLogs(AggregatedLogDateType aggregatedLogDateType)
+        {
+            try
+            {
+                var aggregatedLogs = await _metricsService.GetAggregatedLogs(aggregatedLogDateType);
+
+                return Ok(aggregatedLogs);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
