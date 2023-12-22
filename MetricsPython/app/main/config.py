23de0a8e93e776ffc1978fee5d9397d254a3cc27
paxer2k <alex.arkhipov.7590@gmail.com>
2023-12-22 19:47:31 +0100

    test

--- MetricsPython/app/main/config.py
+++ MetricsPython/app/main/config.py
@@ -7,7 +7,7 @@ class Config:
     RESTX_MASK_SWAGGER = False
     # LOCAL CONNECTION STRING
     #SQLALCHEMY_DATABASE_URI=f"mssql://{os.getenv('MSSQL_USER')}/{os.getenv('MSSQL_DB')}?driver={os.getenv('MSSQL_DRIVER')}" 
-    # AZURE DB CONNECTION STRING
+    # AZURE DB CONNECTION STRING TESTING PIPELINE
     SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc://{os.environ.get('AZURE_SQL_METRICS_UID')}:{os.environ.get('AZURE_SQL_METRICS_PWD')}@{os.environ.get('AZURE_SQL_SERVER')}/{os.environ.get('AZURE_SQL_METRICS_DB')}?driver={os.environ.get('AZURE_SQL_METRICS_DRIVER')}"
     FLASK_ENV=os.environ.get('FLASK_ENV')
     RESTX_ERROR_404_HELP = False
