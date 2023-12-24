edc224fbc51b4bc127a05dae63eaac4f894e0ab9
paxer2k <alex.arkhipov.7590@gmail.com>
2023-12-24 18:29:06 +0100

    db connectionstring adjustment

--- MetricsPython/app/main/config.py
+++ MetricsPython/app/main/config.py
@@ -8,7 +8,7 @@ class Config:
     # LOCAL CONNECTION
     #SQLALCHEMY_DATABASE_URI=f"mssql://{os.getenv('MSSQL_USER')}/{os.getenv('MSSQL_DB')}?driver={os.getenv('MSSQL_DRIVER')}" 
     # AZURE DB CONNECTION STRING
-    SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc://{os.environ.get('AZURE_SQL_METRICS_UID')}:{os.environ.get('AZURE_SQL_METRICS_PWD')}@{os.environ.get('AZURE_SQL_SERVER')}/{os.environ.get('AZURE_SQL_METRICS_DB')}?driver={os.environ.get('AZURE_SQL_METRICS_DRIVER')}"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('METRICS_DB_CONNECTION_STRING')
     FLASK_ENV=os.environ.get('FLASK_ENV')
     RESTX_ERROR_404_HELP = False
 
