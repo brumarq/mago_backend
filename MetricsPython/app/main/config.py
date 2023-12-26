f7f1ce10e1ba6ca51f11142f66b5ba4868aa50d5
paxer2k <alex.arkhipov.7590@gmail.com>
2023-12-26 19:52:55 +0100

    key adjustment

--- MetricsPython/app/main/config.py
+++ MetricsPython/app/main/config.py
@@ -3,11 +3,10 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG = False
-    # Swagger UI #1
+    # Swagger UI
     RESTX_MASK_SWAGGER = False
     # LOCAL CONNECTION
     #SQLALCHEMY_DATABASE_URI=f"mssql://{os.getenv('MSSQL_USER')}/{os.getenv('MSSQL_DB')}?driver={os.getenv('MSSQL_DRIVER')}" 
-    # AZURE DB CONNECTION STRING
     SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc://{os.environ.get('METRICS_DB_CONNECTION_STRING_SQLALCHEMY')}"
     FLASK_ENV=os.environ.get('FLASK_ENV')
     RESTX_ERROR_404_HELP = False
