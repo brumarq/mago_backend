053db86cb30505d98637eec54f0667e04a60b958
Bruno Coimbra Marques <brunocm@pm.me>
2023-12-27 18:53:29 +0100

    make swagger work in production

--- NotificationMS/WebApp/Program.cs
+++ NotificationMS/WebApp/Program.cs
@@ -89,15 +89,9 @@ builder.WebHost.UseUrls($"http://*:{httpPort}");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
