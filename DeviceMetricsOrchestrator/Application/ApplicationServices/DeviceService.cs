16d709922e6a3bd09b8135809dbe3a2fbcf632a1
paxer2k <alex.arkhipov.7590@gmail.com>
2023-12-30 00:55:47 +0100

    authentication now works between metrics/device ms and device-metrics-orchestrator

--- DeviceMetricsOrchestrator/Application/ApplicationServices/DeviceService.cs
+++ DeviceMetricsOrchestrator/Application/ApplicationServices/DeviceService.cs
@@ -2,8 +2,10 @@ using Application.ApplicationServices.Interfaces;
 using Application.DTOs.Device;
 using Application.DTOs.Metrics;
 using Application.Exceptions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace Application.ApplicationServices;
@@ -14,20 +16,26 @@ public class DeviceService : IDeviceService
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly HttpClient _httpClient;
     private readonly string _baseUri;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly string _token;
 
-    public DeviceService(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+    public DeviceService(IConfiguration configuration, IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
     {
         _configuration = configuration;
         _httpClientFactory = httpClientFactory;
         _httpClient = httpClientFactory.CreateClient();
         _baseUri = _configuration["ApiRequestUris:DeviceBaseUri"]!;
+        _httpContextAccessor = httpContextAccessor;
+        _token = _httpContextAccessor.HttpContext.Request.Headers["Authorization"].ToString().Split(" ")[1];
     }
 
     public async Task<bool> DeviceExistsAsync(int deviceId)
     {
         try
         {
-            var response = await _httpClient.GetAsync($"{_baseUri}{deviceId}");
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUri}{deviceId}");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+            var response = await _httpClient.SendAsync(request);
 
             return response.IsSuccessStatusCode && response.StatusCode != HttpStatusCode.NotFound;
         }
@@ -43,7 +51,10 @@ public class DeviceService : IDeviceService
         if (!await DeviceExistsAsync(deviceId))
             throw new NotFoundException($"Device with id {deviceId} does not exist!");
 
-        var response = await _httpClient.GetAsync($"{_baseUri}{deviceId}");
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUri}{deviceId}");
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+        var response = await _httpClient.SendAsync(request);
+
         response.EnsureSuccessStatusCode();
 
         var body = await response.Content.ReadFromJsonAsync<DeviceResponseDTO>();
--- DeviceMetricsOrchestrator/Application/ApplicationServices/MetricsService.cs
+++ DeviceMetricsOrchestrator/Application/ApplicationServices/MetricsService.cs
@@ -2,7 +2,11 @@ using Application.ApplicationServices.Interfaces;
 using Application.DTOs.Device;
 using Application.DTOs.Metrics;
 using Application.Exceptions;
+using Domain.Enums;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace Application.ApplicationServices
@@ -13,13 +17,17 @@ namespace Application.ApplicationServices
         private readonly HttpClient _httpClient;
         private readonly IDeviceService _deviceService;
         private readonly string _baseUri;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string _token;
 
-        public MetricsService(IConfiguration configuration, IHttpClientFactory httpClientFactory, IDeviceService deviceService)
+        public MetricsService(IConfiguration configuration, IHttpClientFactory httpClientFactory, IDeviceService deviceService, IHttpContextAccessor httpContextAccessor)
         {
             _httpClientFactory = httpClientFactory;
             _httpClient = httpClientFactory.CreateClient();
             _deviceService = deviceService;
             _baseUri = configuration["ApiRequestUris:MetricsBaseUri"]!;
+            _httpContextAccessor = httpContextAccessor;
+            _token = _httpContextAccessor.HttpContext.Request.Headers["Authorization"].ToString().Split(" ")[1];
         }
 
         public async Task<IEnumerable<MetricsResponseDTO>> GetMetricsForDeviceAsync(int deviceId)
@@ -27,7 +35,10 @@ namespace Application.ApplicationServices
             if (!await _deviceService.DeviceExistsAsync(deviceId))
                 throw new NotFoundException($"Device with id {deviceId} does not exist!");
 
-            var response = await _httpClient.GetAsync($"{_baseUri}devices/{deviceId}");
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUri}devices/{deviceId}");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+            var response = await _httpClient.SendAsync(request);
+
             response.EnsureSuccessStatusCode();
 
             var body = await response.Content.ReadFromJsonAsync<IEnumerable<MetricsResponseDTO>>();
