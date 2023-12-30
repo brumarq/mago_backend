26dab61fd1d520f17f349bf2554002f9346accce
Bruno Coimbra Marques <brunocm@pm.me>
2023-12-30 18:43:08 +0100

    added necessary things to deploy

--- UserDeviceNotificationOrchestrator/WebApp/Program.cs
+++ UserDeviceNotificationOrchestrator/WebApp/Program.cs
@@ -19,21 +19,16 @@ builder.Services.AddScoped<IDeviceService, DeviceService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddHttpClient();
 
+var httpPort = Environment.GetEnvironmentVariable("HTTP_PORT") ?? "8484";
+builder.WebHost.UseUrls($"http://*:{httpPort}");
+builder.Configuration.AddEnvironmentVariables();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseMetricServer(url: "/metrics");
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
