e4cdc153c29cf2a63d9ff90211cadacc082dc851
paxer2k <alex.arkhipov.7590@gmail.com>
2023-12-30 19:41:28 +0100

    temp fix

--- DeviceMS/Application/DTOs/Misc/UnitDTO.cs
+++ DeviceMS/Application/DTOs/Misc/UnitDTO.cs
@@ -6,5 +6,5 @@ public class UnitDTO : BaseDTO
     public string? Symbol { get; set; }
     public float? Factor { get; set; }
     public float? Offset { get; set; }
-    public QuantityDTO? Quantity { get; set; }
+    /*public QuantityDTO? Quantity { get; set; }*/ //empty for not
 }
