0cb5e5069a1005b3ed0d2335c1742bf7970c2b43
Bruno Marques <44119479+brumarq@users.noreply.github.com>
2023-12-31 01:42:28 +0100

    Delete MetricsMS directory

--- MetricsMS/Domain/Entities/LogCollection.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Entities;
-
-public class LogCollection : BaseEntity
-{
-    public int? DeviceId { get; set; }
-    public DateTime Timestamp { get; set; }
-    public ICollection<LogValue>? Values { get; set; } = new List<LogValue>();
-    public LogCollectionType? LogCollectionType { get; set; }
-}
--- MetricsMS/Domain/Enums/AggregatedLogDateType.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Domain.Enums
-{
-    public enum AggregatedLogDateType
-    {
-        Weekly, Monthly, Yearly
-    }
-}
