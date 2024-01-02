6afbe66ca3d559bfda652d0c4f8cd08ecb5c493d
paxer2k <alex.arkhipov.7590@gmail.com>
2024-01-02 10:24:18 +0100

    name change

--- ProcessAggregationLogsTimer/function_app.py
+++ ProcessAggregationLogsTimer/function_app.py
@@ -4,14 +4,14 @@ from service import AggregatedLogsProcessor
 
 app = func.FunctionApp()
 
-@app.function_name(name="calc_averages_timer")
-@app.schedule(schedule="* * * * *", arg_name="myTimer", run_on_startup=True,
+@app.function_name(name="process_aggregation_logs_timer")
+@app.schedule(schedule="0 5 * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
-def calc_averages_timer(myTimer: func.TimerRequest) -> None:
+def process_aggregation_logs_timer(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
     aggregated_logs_processor = AggregatedLogsProcessor()
     aggregated_logs_processor.perform_daily_processing()
 
-    logging.info('Python timer trigger function executed.')
+    logging.info('Daily aggregation processing has been performed.')
