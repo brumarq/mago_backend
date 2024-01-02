2f9e132b3f16698924e9cbf60f24856625a9b460
paxer2k <alex.arkhipov.7590@gmail.com>
2024-01-02 01:46:07 +0100

    finished logic for azure function

--- CalculateAveragesFunction/function_app.py
+++ CalculateAveragesFunction/function_app.py
@@ -1,16 +1,16 @@
 import logging
 import azure.functions as func
-from queries import calculate_and_save_aggregates
+from queries import perform_daily_processing
 
 app = func.FunctionApp()
 
-@app.function_name(name="CalculateAveragesTimer")
+@app.function_name(name="calc_averages_timer")
 @app.schedule(schedule="* * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
-def CalculateAveragesTimer(myTimer: func.TimerRequest) -> None:
+def calc_averages_timer(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    calculate_and_save_aggregates()
+    perform_daily_processing()
 
     logging.info('Python timer trigger function executed.')
