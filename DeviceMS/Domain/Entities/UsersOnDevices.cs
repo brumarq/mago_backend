b980e2670c4d9c38ae234f209c30d159c0e98912
REDitor <sander@trone.nl>
2024-01-04 15:13:23 +0100

    Restructured UsersOnDevices.cs

--- DeviceMS/Domain/Entities/UsersOnDevices.cs
+++ DeviceMS/Domain/Entities/UsersOnDevices.cs
@@ -3,6 +3,6 @@ namespace Domain.Entities;
 public class UsersOnDevices : BaseEntity
 {
     public string? UserId { get; set; }
-    public int DeviceId { get; set; } // for mapping
+    public int DeviceId { get; set; } // navigation prop
     public Device? Device { get; set; }
 }
