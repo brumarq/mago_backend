be0e914f80c5663e58993a77a9f3b3dc1b4f440b
paxer2k <alex.arkhipov.7590@gmail.com>
2024-01-04 16:15:45 +0100

    123

--- ProcessAggregationLogsTimer/function_app.py
+++ ProcessAggregationLogsTimer/function_app.py
@@ -5,8 +5,8 @@ from service import AggregatedLogsProcessor
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.function_name(name="process_aggregation_logs_timer")
-@app.schedule(schedule="5 0 * * *", arg_name="myTimer", run_on_startup=True, # Daily timer at 12:05AM (00:05) // another test v2
-              use_monitor=False) 
+@app.schedule(schedule="5 0 * * *", arg_name="myTimer", run_on_startup=True, # Daily timer at 12:05AM (00:05)
+              use_monitor=True) 
 def process_aggregation_logs_timer(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
