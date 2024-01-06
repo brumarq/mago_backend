5dd48f52c1a945ca9a6d13267254f1943dec7179
paxer2k <alex.arkhipov.7590@gmail.com>
2024-01-06 17:03:18 +0100

    desc

--- DeviceMetricsOrchestrator/Application/Helpers/HttpRequestHelper.cs
+++ DeviceMetricsOrchestrator/Application/Helpers/HttpRequestHelper.cs
@@ -5,20 +5,25 @@ namespace Application.Helpers
 {
     public static class HttpRequestHelper
     {
+        /// <summary>
+        /// This method is made to read out Python exceptions in C# as they are differently handled.
+        /// </summary>
+        /// <param name="response"></param>
+        /// <exception cref="HttpRequestException"></exception>
         public static void CheckStatusAndParseErrorMessageFromJsonData(HttpResponseMessage response)
         {
             if (response.StatusCode != HttpStatusCode.OK)
             {
                 if (response.Content.Headers.ContentLength > 0)
                 {
-                    String respStr = response.Content.ReadAsStringAsync().Result;
+                    string respStr = response.Content.ReadAsStringAsync().Result;
                     var dic = JsonConvert.DeserializeObject<Dictionary<string, string>>(respStr);
                     string message = dic["message"];
                     response.ReasonPhrase = message;
                 }
                 throw new HttpRequestException(response.ReasonPhrase, inner: null, response.StatusCode);
             }
-            response.EnsureSuccessStatusCode();
+            response.EnsureSuccessStatusCode(); // just in case this fails, use normal way of handling messages...
         }
     }
 }
