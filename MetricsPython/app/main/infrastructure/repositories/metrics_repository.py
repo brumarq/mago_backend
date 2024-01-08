46cb6c7c2c4c0129ee9c403faa943c94633bbf5a
paxer2k <alex.arkhipov.7590@gmail.com>
2024-01-08 15:26:25 +0100

    query fix

--- MetricsPython/app/main/infrastructure/repositories/metrics_repository.py
+++ MetricsPython/app/main/infrastructure/repositories/metrics_repository.py
@@ -9,19 +9,28 @@ class MetricsRepository(Repository):
 
     def get_latest_log_values_by_device_id(self, device_id):
         try:
-            latest_timestamp_subquery = ( # Get latest timestamps (created_at) for each Field in a Device
+            latest_timestamp_subquery = (
                 db.session.query(
                     LogValue.field_id,
                     func.max(LogValue.created_at).label('latest_timestamp')
                 )
+                .join(LogCollection)
+                .filter(LogCollection.device_id == device_id)
                 .group_by(LogValue.field_id)
                 .subquery()
             )
 
-            return self.model.query.join(latest_timestamp_subquery, and_( # Return all unique entries with latest timestamps 
-                LogValue.field_id == latest_timestamp_subquery.c.field_id,
-                LogValue.created_at == latest_timestamp_subquery.c.latest_timestamp
-            )).join(LogCollection).filter(LogCollection.device_id == device_id).all()
+            # Query the LogValue entries using the latest timestamps and device_id
+            latest_log_values = self.model.query.join(
+                latest_timestamp_subquery,
+                and_(
+                    LogValue.field_id == latest_timestamp_subquery.c.field_id,
+                    LogValue.created_at == latest_timestamp_subquery.c.latest_timestamp,
+                ),
+            ).join(LogCollection).filter(LogCollection.device_id == device_id).all()
+
+            return latest_log_values
+        
         except SQLAlchemyError as e:
             db.session.rollback()
             raise e
