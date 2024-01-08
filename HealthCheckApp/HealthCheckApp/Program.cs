fae8959234f43d0fdb82c2c49b0df7fd5f391630
REDitor <sander@trone.nl>
2024-01-08 13:06:56 +0100

    Created Health Check App

---
+++ HealthCheckApp/HealthCheckApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Sockets;
+
+namespace HealthCheckApp;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var servicesToCheck = new (string Host, int Port)[]
+        {
+            ("firmware-microservice-service-mago-backend-test.apps.ocp4-inholland.joran-bergfeld.com", 6969)
+        };
+
+        foreach (var service in servicesToCheck)
+        {
+            await CheckServiceHealth(service.Host, service.Port);
+        }
+    }
+
+    static async Task CheckServiceHealth(string host, int port)
+    {
+        using var client = new TcpClient();
+        try
+        {
+            await client.ConnectAsync(host, port);
+            Console.WriteLine($"Success: Connected to {host}:{port}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
