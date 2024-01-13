48cf0155fe57a7f625a4747c02fac0464ebac0e0
paxer2k <alex.arkhipov.7590@gmail.com>
2024-01-13 22:21:33 +0100

    some new stuff

--- MetricsPython/app/main/webapp/controller/probe_controller.py
+++ MetricsPython/app/main/webapp/controller/probe_controller.py
@@ -3,6 +3,7 @@ from flask import Response
 from app.main.application.dtos.probe_resource_namespace import ReadyResourceNameSpace
 from app.main.application.dtos.probe_resource_namespace import HealthResourceNameSpace
 from app.main.application.service.probe_service import ProbeService
+from app.main.webapp.custommetrics.application_state import set_readiness_status, set_health_status
 
 health_api = HealthResourceNameSpace.api
 ready_api = ReadyResourceNameSpace.api
@@ -19,12 +20,19 @@ class ReadyResource(Resource):
     def get(self):
         is_database_up = probe_service.is_database_up()
         is_migration_successful = probe_service.is_migration_successful()
-        return is_database_up and is_migration_successful
-        
+
+        if (is_database_up and is_migration_successful):
+            set_readiness_status(True)
+            return {'status': 'ready'}, 200
+        else:
+            set_readiness_status(False)
+            return {'status': 'failed to apply pending database migrations'}, 503
+            
 
 @health_api.route('', doc=False)
 @health_api.response(200, 'Successful ping to the endpoint')
 class HealthResource(Resource):
     @health_api.doc('health', doc=False)
     def get(self):
-        return Response(status=200, response='')
+        set_health_status(True)
+        return {'status': 'up'}, 200
