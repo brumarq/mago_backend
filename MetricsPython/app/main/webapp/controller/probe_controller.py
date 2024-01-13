bc0ab58fced61e311a5674313e4bf0b26fe35022
paxer2k <alex.arkhipov.7590@gmail.com>
2024-01-13 22:50:45 +0100

    added extra check

--- MetricsPython/app/main/webapp/controller/probe_controller.py
+++ MetricsPython/app/main/webapp/controller/probe_controller.py
@@ -1,5 +1,5 @@
 from flask_restx import Resource
-from flask import Response
+from flask import jsonify
 from app.main.application.dtos.probe_resource_namespace import ReadyResourceNameSpace
 from app.main.application.dtos.probe_resource_namespace import HealthResourceNameSpace
 from app.main.application.service.probe_service import ProbeService
@@ -21,14 +21,18 @@ class ReadyResource(Resource):
         is_database_up = probe_service.is_database_up()
         is_migration_successful = probe_service.is_migration_successful()
 
-        if (is_database_up and is_migration_successful):
-            set_readiness_status(True)
-            return {'status': 'ready'}, 200
-        else:
+        if not is_database_up:
             set_readiness_status(False)
-            return {'status': 'failed to apply pending database migrations'}, 503
-            
+            return {"status": "could not connect to database."}, 503
 
+        if not is_migration_successful:
+            set_readiness_status(False)
+            return {"status": "failed to apply pending database migrations"}, 503
+
+        set_readiness_status(True)
+        return {'status': 'ready'}, 200
+            
+            
 @health_api.route('', doc=False)
 @health_api.response(200, 'Successful ping to the endpoint')
 class HealthResource(Resource):
