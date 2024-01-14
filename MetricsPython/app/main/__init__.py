c4e2e4cd884dcfc2864b0f8a00ccf279f85765f3
paxer2k <alex.arkhipov.7590@gmail.com>
2024-01-14 13:27:42 +0100

    another test

--- MetricsPython/app/main/__init__.py
+++ MetricsPython/app/main/__init__.py
@@ -2,13 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.main.config import config_by_name
 from flask.app import Flask
+from flask_apscheduler import APScheduler
 
 db = SQLAlchemy()
+scheduler = APScheduler()
 
 def create_app(config_name: str) -> Flask:
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
+    scheduler.init_app(app)
     
     return app
 
