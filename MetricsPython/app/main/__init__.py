8f0ae5a1b00a4bc35338bfa3543749e319433540
paxer2k <alex.arkhipov.7590@gmail.com>
2024-01-14 21:41:28 +0100

    123

--- MetricsPython/app/main/__init__.py
+++ MetricsPython/app/main/__init__.py
@@ -3,13 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from app.main.config import config_by_name
 from flask.app import Flask
 import logging
+import sys
 
 db = SQLAlchemy()
 
 def create_app(config_name: str) -> Flask:
     app = Flask(__name__)
-    app.logger.setLevel(logging.INFO)
     app.config.from_object(config_by_name[config_name])
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.INFO)
     db.init_app(app)
     
     return app
