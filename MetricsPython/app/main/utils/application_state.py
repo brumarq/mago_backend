3a2ae917bfb2af09d3cb40ee307d73b5c80f4a4d
paxer2k <alex.arkhipov.7590@gmail.com>
2024-01-21 18:23:25 +0100

    last try

--- MetricsPython/app/main/utils/application_state.py
+++ MetricsPython/app/main/utils/application_state.py
@@ -1,4 +1,5 @@
 from prometheus_client import Gauge, Histogram, Counter
+import psutil
 
 """
 Methods for setting health and readiness status | Application state tracking
@@ -10,6 +11,18 @@ READINESS_STATUS = Gauge('application_readiness_status', 'Readiness status of th
 # Custom Prometheus metrics for HTTP requests
 HTTP_REQUEST_DURATION = Histogram('http_request_duration_seconds', 'Duration of HTTP requests in seconds', labelnames=['method', 'status_code'])
 HTTP_REQUEST_COUNTER = Counter('http_request_total', 'Total count of HTTP requests', labelnames=['method', 'status_code'])
+
+PROCESS_RESIDENT_MEMORY_BYTES = Gauge(
+    'process_resident_memory_bytes',
+    'Resident memory size of the process in bytes'
+)
+
+def update_resident_memory_bytes():
+    process = psutil.Process()
+    resident_memory_bytes = process.memory_info().rss
+    PROCESS_RESIDENT_MEMORY_BYTES.set(resident_memory_bytes)
+    
+update_resident_memory_bytes()
 
 # Methods for setting health and readiness status
 def set_health_status(is_healthy):
