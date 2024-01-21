e6a84eb1746e68f2062594b0d9df7af117212de1
paxer2k <alex.arkhipov.7590@gmail.com>
2024-01-21 18:12:41 +0100

    yes

--- MetricsPython/app/main/utils/application_state.py
+++ MetricsPython/app/main/utils/application_state.py
@@ -11,9 +11,6 @@ READINESS_STATUS = Gauge('application_readiness_status', 'Readiness status of th
 HTTP_REQUEST_DURATION = Histogram('http_request_duration_seconds', 'Duration of HTTP requests in seconds', labelnames=['method', 'status_code'])
 HTTP_REQUEST_COUNTER = Counter('http_request_total', 'Total count of HTTP requests', labelnames=['method', 'status_code'])
 
-# Custom Metric for process resident memory in bytes
-PROCESS_RESIDENT_MEMORY_IN_BYTES = Gauge('process_resident_memory_in_bytes', 'Resident memory size of the process in bytes')
-
 # Methods for setting health and readiness status
 def set_health_status(is_healthy):
     HEALTH_STATUS.set(1 if is_healthy else 0)
