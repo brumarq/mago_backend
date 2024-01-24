a407f1cfe790811ac6861505b5503d10c57bf85d
paxer2k <alex.arkhipov.7590@gmail.com>
2024-01-24 17:43:53 +0100

    13321

--- MetricsPython/log_config.py
+++ MetricsPython/log_config.py
@@ -1,33 +1,8 @@
 import logging
-from logging.config import dictConfig
 
-# Configure the logging settings
-log_config = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'stdout': {
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stdout',
-            'formatter': 'default',
-        },
-    },
-    'formatters': {
-        'default': {
-            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        },
-    },
-    'root': {
-        'level': 'INFO',
-        'handlers': ['stdout'],
-    },
-    'loggers': {
-        'werkzeug': {
-            'level': 'INFO',
-            'handlers': ['stdout'],
-            'propagate': False,
-        },
-    },
-}
+# Configure the root logger
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-dictConfig(log_config)
+# Silence the excessive loggers
+logging.getLogger('werkzeug').setLevel(logging.INFO)
